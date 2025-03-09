a931289b773b5d3a10f9e6c49aa40779dbc4aa1c
abdullahmiraz <abdullahmiraz2015@gmail.com>
2025-03-09 10:22:31 +0600

    uploaded with env file cleaned

--- src/lib/firebase.config.ts
+++ src/lib/firebase.config.ts
@@ -16,12 +16,12 @@ import { getMessaging, getToken, isSupported } from "firebase/messaging";
 // };
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCOBXGAWycr77-X3kTZx8yg5s9sKUQhj4E",
-  authDomain: "native-pushy-android.firebaseapp.com",
-  projectId: "native-pushy-android",
-  storageBucket: "native-pushy-android.firebasestorage.app",
-  messagingSenderId: "318700872830",
-  appId: "1:318700872830:web:a03f34a33d5646afe3242f",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
@@ -42,9 +42,7 @@ export const generateToken = async () => {
   console.log(permission);
   if (permission === "granted") {
     const token = await getToken(messaging, {
-      vapidKey:
-        "BON3glLAzJE1jUgqsMB4xiW1x-bZrhcpL1yctPns19aAvT9BsxOyoG-YRv-6bi6T1Lu6tSlwMGrOntVHKo_dNgU",
-      // "BJF_FiFX39onHaWHea4lrF5PizRRdA_W4VYYsW89LSMq-vK6OoOEvhh-um8HFuTzOd5HJjPFbiyCOnN9z2Ykhok",
+      vapidKey: process.env.NEXT_PUBLIC_FIREBASE_VAPID_KEY,
     });
     console.log("Token: ", token);
     return token;
